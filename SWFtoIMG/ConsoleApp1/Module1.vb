b89a97996da4b6c832302ac3a8622c4f757994bd
YUCHAN MO <mojjijji@gmail.com>
2019-06-07 13:01:06 +0900

    download swf and convert it to png

---
+++ SWFtoIMG/ConsoleApp1/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim SWFToImage = CreateObject(“SWFToImage.SWFToImageObject”)
+
+        SWFToImage.InitLibrary(“demo”, “demo”)
+
+        Dim WshShell, fs, f, fc, f1, tsFile
+        Dim WshShell = WScript.CreateObject(“WScript.Shell”)
+
+        sCurrentFolder = WshShell.CurrentDirectory
+
+Set fs = CreateObject(“Scripting.FileSystemObject”)
+Set f = fs.GetFolder(sCurrentFolder) ' current directory
+Set fc = f.Files
+Set fs = Nothing
+
+' converting SWF files to JPG image files
+For Each f1 In fc
+            If UCase(Mid(f1.name, InStrRev(f1.name, “.”) + 1)) = “SWF” Then
+                SWFToImage.InputSWFFileName = f1.name
+                SWFToImage.FrameIndex = 1 ' number of frame to extract
+                SWFToImage.ImageOutputType = 1 ' set output image type to Jpeg (0 = BMP, 1 = JPG, 2 = GIF)
+                SWFToImage.Execute
+                SWFToImage.SaveToFile Mid(f1.name, 1, InStr(f1.name, “.”) – 1) & “.jpg”
+End If
+
+        Next
+
+Set SWFToImage = nothing
+    End Sub
+
+End Module
---
+++ SWFtoIMG/ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swfilelist = System.IO.Directory.GetFiles(@"C:\CODE\guitarcrawler\music").Where((v) => v.Contains(".swf"));
+
+            foreach (var file in swfilelist)
+            {
+                var inputfile = new FileInfo(file);
+                var imgfile = inputfile.Name.Split('.')[0] + ".png";
+                var imgfullpath = System.IO.Path.Combine(@"C:\music\png", imgfile);
+                //Console.WriteLine(imgfullpath);
+                Console.WriteLine($"start to convert {inputfile} ");
+                //var ouputpath = @"C:\music\가거라.png";
+                ConvertSWFToPng(inputfile.FullName, imgfullpath);
+            }
+
+        }
+
+        public static void ConvertSWFToPng(string _inputPath, string _otputPath, int width = 1500, int height = 1000)
+        {
+            if (string.IsNullOrEmpty(_inputPath) || string.IsNullOrEmpty(_otputPath))
+            {
+                throw new ArgumentNullException("ConvertSWFToJpg(string _inputPath,string _otputPath)");
+            }
+            if (!System.IO.File.Exists(_inputPath))
+                throw new Exception("Файл для конвертации в формат jpg не найден: " + _inputPath);
+
+            SWFToImage.SWFToImageObject obj = new SWFToImage.SWFToImageObject();
+            obj.InitLibrary("demo", "demo");
+            obj.ImageOutputType = SWFToImage.TImageOutputType.iotPNG;
+            obj.JPEGQuality = 100;
+            obj.ImageWidth = width;
+            obj.ImageHeight = height;
+
+            obj.InputSWFFileName = _inputPath;
+
+            try
+            {
+                obj.Execute_Begin();
+
+                obj.FrameIndex = 0;
+                obj.Execute_GetImage();
+                obj.SaveToFile(_otputPath);
+                obj.Execute_End();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка конвертации из формата swf в формат png: " + ex.Message);
+            }
+        }
+    }
+}
